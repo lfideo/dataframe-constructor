458c12f779983c38c30212dac72a525ba8ca8cf1
Ilya Lapshin <ilyaa.lapshin@gmail.com>
2023-01-06 14:16:05 +0300

    Initial version of the file

---
+++ df_builder.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import glob
+import pandas as pd
+import numpy as np
+
+os.getcwd()
+
+# changes current directory
+os.chdir('/Users/lfideo/Downloads')
+
+# lists files in cwd
+# os.listdir()
+for file in os.listdir():
+    if file.find('data')==0 and len(file)<=20:        
+        shutil.move(file, "/Users/lfideo/Desktop/***/crm/data")
+
+# change the cwd
+os.chdir("/Users/lfideo/Desktop/***/crm/data")   
+
+# define the path
+path = os.getcwd()
+file_list = glob.glob(path + '/*.csv')     
+
+li = []
+for file in file_list:
+    df = pd.read_csv(file, dtype='object', index_col=None, header=0)
+    li.append(df)
+
+crm = pd.concat(li, axis=0, ignore_index=True)
+
+crm.head()
